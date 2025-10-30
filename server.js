58d39cfcdffe1b7fea2a206d9a86447a19f654da
Craig Liu <yliu7029@uni.sydney.edu.au>
2025-10-30 19:16:31 +1100

    config port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import next from "next";
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-
+const PORT = process.env.PORT || 3000;
 const httpsOptions = {
   key: fs.readFileSync("./certs/server.key"),
   cert: fs.readFileSync("./certs/server.pem"),
@@ -22,7 +22,8 @@ app.prepare().then(() => {
       // only accepy https https://localhost:3000
       return handle(req, res);
     })
-    .listen(3000, () => {
-      console.log("🚀 HTTPS Server ready at https://localhost:3000");
+    .listen(PORT, () => {
+      console.log(`🚀 HTTPS Server ready at https://localhost:${PORT}`);
     });
+    
 });
